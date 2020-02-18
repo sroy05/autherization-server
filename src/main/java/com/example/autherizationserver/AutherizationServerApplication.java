8b074c52255256164a01991539b892fab96fa2f5
Ray <sulata.ray@accenture.com>
2020-02-18 16:56:21 +0530

    authserver creation

--- src/main/java/com/example/autherizationserver/AutherizationServerApplication.java
+++ src/main/java/com/example/autherizationserver/AutherizationServerApplication.java
@@ -2,8 +2,14 @@ package com.example.autherizationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses= {com.example.service.UserDetailsServiceImpl.class,com.example.configuration.AuthServerSecurityConfigure.class})
+@EnableJpaRepositories(basePackages= {"com.example.model","com.example.repository"})
+@EntityScan("com.example.model")
+@EnableAuthorizationServer
 public class AutherizationServerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/service/UserDetailsServiceImpl.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.example.model.AccountAuth;
 import com.example.model.User;
 import com.example.repository.UserRepository;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService{
 	
 	@Autowired
